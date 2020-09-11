2860ea65b221db0063efff389b02833dfba1722f
den <denys.kozii@gmail.com>
2020-09-11 10:13:24 +0300

    added Redis dependencies and Beans

--- src/main/java/com/denyskozii/meetingcounter/MeetingCounterApplication.java
+++ src/main/java/com/denyskozii/meetingcounter/MeetingCounterApplication.java
@@ -2,9 +2,24 @@ package com.denyskozii.meetingcounter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class MeetingCounterApplication {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate<Integer, Integer> redisTemplate() {
+        RedisTemplate<Integer, Integer> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MeetingCounterApplication.class, args);
